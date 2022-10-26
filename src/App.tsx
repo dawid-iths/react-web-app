d0f2c854832bdd4baec6243dd5f7c7473649cbfc
dawid-ql <dawid@queenslab.se>
2022-10-26 16:16:55 +0200

    added LogRocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ClickEventButton from './components/button';
+import LogRocket from 'logrocket';
+LogRocket.init('igsknz/logrocket-demo-iths');
 
 function App() {
   const ClickMe = () => {
@@ -10,7 +11,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           ITHS 
         </p>
