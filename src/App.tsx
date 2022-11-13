bbc538099118be1743423e79be7a2c36be802bc2
dawid-ql <dawid@queenslab.se>
2022-11-13 21:42:34 +0100

    npm fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
-import React from 'react';
 import './App.css';
-import ClickEventButton from './components/button';
-import FragmentExample from './components/FragmentExample';
-import CdnImage from './components/ImageFromCDN';
-
-
 
 function App() {
-  const ClickMe = () => {
-    console.log("change log text");
-  };
   return (
     <div className="App">
       <header className="App-header">
@@ -19,9 +10,6 @@ function App() {
         <p>
         Envoronment: {process.env.NODE_ENV}
         </p>
-        {/* <FragmentExample></FragmentExample>
-        <CdnImage imgUrl="https://res.cloudinary.com/ql-cdn-dev/image/upload/w_300,h_200,c_scale/MIT-Coding-Brain-01-press_0.jpg"></CdnImage>
-  <ClickEventButton onClick={ClickMe}>Klicka!</ClickEventButton>*/}
       </header> 
     </div>
   );
